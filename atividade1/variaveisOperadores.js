1d6249f07132c02ac65d6f5e621fdc8f322d677c
EvertonPL <evertonpl2013@gmail.com>
2026-03-08 23:28:03 -0300

    criação de operadores

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,13 @@
+
+
+
+a = 10
+b = 2
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão =  a / b
+console.log(`soma de ${a} + ${b}: ${soma}`)
+console.log(`Subtração de ${a} - ${b}: ${subtração}`)
+console.log(`multiplicação de ${a} * ${b}: ${multiplicação}`)
+console.log(`divisão de ${a} / ${b}: ${divisão}`)
