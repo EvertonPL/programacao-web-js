bd035ab76d83488ade2168b58bd75e6cb1499dad
EvertonPL <evertonpl2013@gmail.com>
2026-03-30 20:42:38 -0300

    desenvolvendo calculadora web

--- appweb_calculadora/index.js
+++ appweb_calculadora/index.js
@@ -1,15 +1,31 @@
 // carrega framework express
 const express = require('express');
 // cria um app usando a função express()
+const calculadora = require('./calculadora');
 const app     = express();
 const PORT = 8080;
+const LOG = [];
 
-app.get('/', (req, res)=>{
-    res.send('<h1>Olá, mundo!</h1> <p>Estou na web...</p>');
+app.get('/ola/joao', (req, res)=>{
+    res.send('<h1>Olá, joão!</h1> <p>Estou na web...</p>');
 });
 
-app.get('/bem-vindo', (req, res)=>{
-    res.send('<h1>Bem-vindo ao desenvolvimento WEB!<\H1>');
+app.get('/calculadora/somar/:a/:b', (req, res)=>{
+    let a = Number(req.params.a);
+    let b = Number(req.params.b);
+    let resultado = calculadora.somar(a, b);
+    let string_resultado = `<h1>${a} + ${b} = ${resultado}</h1>`;
+    res.send(string_resultado);
+    LOG.push(string_resultado)
+    res.send(string_resultado);
+});
+
+app.get('/calculadora/log', (req, res)=>{
+    resultado = "";
+    LOG.forEach(log => {
+        resultado += log;
+    });
+    res.send(resultado);
 });
 
 
